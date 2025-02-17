0f09d9a22a7e80ec4ebf4abf057fbb2c3ac8869a
ryeong0418 <ryeong2105@gmail.com>
2025-02-17 15:50:52 +0900

    sys path 추가

--- airflow/dags_candlestick_daily.py
+++ airflow/dags_candlestick_daily.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "../plugins"))
+
 from airflow import DAG
 from scripts.upload_to_storage import upload_to_blob_storage
 from scripts.candlestick_data import CandleStick_Daily
