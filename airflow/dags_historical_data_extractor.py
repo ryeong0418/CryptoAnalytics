a3219dbc7c9e43550cac98f856ce61b5a6729bc2
ryeong0418 <ryeong2105@gmail.com>
2025-03-27 17:38:44 +0900

    nx data upload

--- airflow/dags_historical_data_extractor.py
+++ airflow/dags_historical_data_extractor.py
@@ -1,45 +1,45 @@
-from airflow import DAG
-from plugins.operators.DataExtractOperator import DataExtractOperator
-from scripts.upload_to_storage import upload_to_blob_storage
-from airflow.operators.python import PythonOperator
-from airflow.models import Variable
-from airflow.models.baseoperator import BaseOperator
-import pendulum
-import json
-
-with DAG(
-    dag_id='dags_historical_data_extractor',
-    start_date=pendulum.datetime(2023,3,1,tz='Asia/Seoul'),
-    catchup=False,
-    schedule_interval='* * * * *'
-
-) as dag:
-
-    start_date = pendulum.datetime(2024, 6, 1, tz='Asia/Seoul')
-    end_date = pendulum.datetime(2024, 6, 30, tz='Asia/Seoul')
-    specified_date = start_date
-
-    while specified_date < end_date:
-        date_str = specified_date.format("")
-
-        extract_task = DataExtractOperator(
-            task_id="extract_task",
-            http_conn_id="",
-            endpoint="",
-            dag=dag
-        )
-
-        upload_blob_task = PythonOperator(
-            task_id="upload_blob_task",
-            python_callable=upload_to_blob_storage,
-            op_args="",
-            provide_context=True,
-            dag=dag
-        )
-
-        extract_task >> upload_blob_task
-
-        specified_date = specified_date.add(days=1)
+# from airflow import DAG
+# from plugins.operators.DataExtractOperator import DataExtractOperator
+# from scripts.upload_to_storage import upload_to_blob_storage
+# from airflow.operators.python import PythonOperator
+# from airflow.models import Variable
+# from airflow.models.baseoperator import BaseOperator
+# import pendulum
+# import json
+#
+# with DAG(
+#     dag_id='dags_historical_data_extractor',
+#     start_date=pendulum.datetime(2023,3,1,tz='Asia/Seoul'),
+#     catchup=False,
+#     schedule_interval='* * * * *'
+#
+# ) as dag:
+#
+#     start_date = pendulum.datetime(2024, 6, 1, tz='Asia/Seoul')
+#     end_date = pendulum.datetime(2024, 6, 30, tz='Asia/Seoul')
+#     specified_date = start_date
+#
+#     while specified_date < end_date:
+#         date_str = specified_date.format("")
+#
+#         extract_task = DataExtractOperator(
+#             task_id="extract_task",
+#             http_conn_id="",
+#             endpoint="",
+#             dag=dag
+#         )
+#
+#         upload_blob_task = PythonOperator(
+#             task_id="upload_blob_task",
+#             python_callable=upload_to_blob_storage,
+#             op_args="",
+#             provide_context=True,
+#             dag=dag
+#         )
+#
+#         extract_task >> upload_blob_task
+#
+#         specified_date = specified_date.add(days=1)
 
 
 
---
+++ scripts/nx_api_to_json_operator.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+from airflow.models.baseoperator import BaseOperator
+from airflow.hooks.base import BaseHook
+from pprint import pprint
+import logging
+import requests
+from airflow.models import Variable
+from airflow.utils.context import Context
+
+
+class NxApiToJsonOperator(BaseOperator):
+
+    def __init__(self, http_conn_id, endpoint, **kwargs):
+        super().__init__(**kwargs)
+        self.http_conn_id = http_conn_id
+        self.endpoint = endpoint
+
+    def execute(self, context):
+        connection = BaseHook.get_connection(self.http_conn_id)
+        logging.info(f"connection:{connection}")
+        self.URI = f'{connection.host}{self.endpoint}'
+
+        api_key = Variable.get("apikey_openapi_nx")
+        headers = {'x-nxopen-api-key': api_key}
+
+        result = requests.get(self.URI, headers=headers)
+        raw_data = result.json()
+        pprint(raw_data)
+
+        return raw_data
