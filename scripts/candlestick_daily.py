53bf6969292897a0d2e62423c9b2e49b47282356
ryeong0418 <ryeong2105@gmail.com>
2025-04-11 13:58:29 +0900

    time sleep 설정

--- scripts/candlestick_daily.py
+++ scripts/candlestick_daily.py
@@ -2,6 +2,7 @@ from airflow.models.baseoperator import BaseOperator
 from scripts.common.utils import SystemUtils
 import requests
 import json
+import time
 
 
 class CandleStickDailyOperator(BaseOperator):
@@ -18,6 +19,7 @@ class CandleStickDailyOperator(BaseOperator):
         market_list = SystemUtils.get_market_list(self.all_market)
         print("마켓 리스트")
         print(market_list)
+        print(self.execution_date)
 
         for market in market_list:
 
@@ -28,14 +30,15 @@ class CandleStickDailyOperator(BaseOperator):
             }
             headers = {'accept':'application/json'}
             response = requests.get(self.candles_days, params=params, headers=headers)
+            print(response)
 
             if response.status_code != 200:
                 print(f"❌ {market} 실패: {response.status_code}")
                 continue
 
+            time.sleep(1)
             daily_candle_data = response.json()
             data_json = json.dumps(daily_candle_data, indent=4, sort_keys=True, ensure_ascii=False)
             result[market] = data_json
-            print(result[market])
 
         return result
