4f2a629b83b69e9d1558e66851a20e39880ed2e5
ryeong0418 <ryeong2105@gmail.com>
2025-07-16 13:27:11 +0900

    catchup True 설정

--- airflow/task_upload_blob_test.py
+++ airflow/task_upload_blob_test.py
@@ -23,28 +23,6 @@ def get_market_list(**context):
 
     return market_list
 
-    # for market in market_list:
-    #
-    #     to_param = f"{self.execution_date}T00:00:00"
-    #     params = {
-    #         'market': market,
-    #         'to': to_param
-    #     }
-    #     headers = {'accept': 'application/json'}
-    #     response = requests.get(self.candles_days, params=params, headers=headers)
-    #     print(response)
-    #
-    #     if response.status_code != 200:
-    #         print(f"❌ {market} 실패: {response.status_code}")
-    #         continue
-    #
-    #     time.sleep(1)
-    #     daily_candle_data = response.json()
-    #     data_json = json.dumps(daily_candle_data, indent=4, sort_keys=True, ensure_ascii=False)
-    #     result[market] = data_json
-    #     print(result[market])
-    #
-    # return result
 
 with DAG(
     dag_id="task_upload_blob_test",
@@ -52,6 +30,7 @@ with DAG(
     start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
     end_date=pendulum.datetime(2024, 1, 10, tz="Asia/Seoul"),
     max_active_runs=1,
+    catchup=True,
     tags=["upload", "candlestick"],
 ) as dag:
 
